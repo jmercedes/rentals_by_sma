9f1b055366a8583d193d8700ef6215a3cac34dd5
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-03-17 21:12:51 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RentalsBySma::Application.config.secret_token = '244377826b5182c95edf8d102c25f6fc5536421c92673ee8187945b448923264288948283097b106f4af5b1f90ca2b9dd3f6a2f8f5988023093386221c5051dd'
