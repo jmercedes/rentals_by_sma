042886ac09c050d1914b404edb9ed0159cd351ee
Juan Mercedes <j.mercedes@thedesignpill.com>
2013-03-29 23:04:00 -0400

    add comment to assets precompile line of code

--- config/application.rb
+++ config/application.rb
@@ -65,6 +65,7 @@ module RentalsBySma
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
+    # set assets precompile
     config.assets.initialize_on_precompile = true
     
   end
